ce2d984142381003487051bb447055e9411b94dc
Nathan Campbell <nathancampbell@outlook.com>
2017-10-06 19:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'javascript-fetch-lab'
-  fetch(`https://api.github.com/repos/chipsaboy/${repo}/forks`)
+  fetch(`https://api.github.com/repos/chipsaboy/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  })
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return 'be316db742286210dc5a9255f11a20b6ad7607c4'
 }
