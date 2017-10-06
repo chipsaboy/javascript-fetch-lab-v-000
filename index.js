9e6d454536703f590eddea4d27d97cef7efe7334
Nathan Campbell <nathancampbell@outlook.com>
2017-10-06 19:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ function showForkedRepo(res) {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'be316db742286210dc5a9255f11a20b6ad7607c4'
+  return ''
 }
